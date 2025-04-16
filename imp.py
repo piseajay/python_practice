fde395d1382c9a9a53ba61583fe9705b9bceeb80
Ajay Pise <piseajay557@gmail.com>
2025-04-16 21:45:12 +0530

    Import python

---
+++ imp.py
@@ -0,0 +1,19 @@
+
+import math
+from math import sin
+
+import calendar
+import datetime
+import os
+import antigravity	
+
+
+print(math.sin(45))
+print(sin(100))
+
+print(datetime.date.today())
+print(calendar.isleap(2020))
+print(os.getcwd())
+print(os.__file__)
+
+number: int = 5
